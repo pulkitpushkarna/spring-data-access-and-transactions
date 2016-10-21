d006e3d497ede630b93c6aebeed5f258e9cb11b5
pulkit <pulkit.pushkarna@gmail.com>
2016-10-21 17:12:48 +0530

    basic configuration done

--- src/main/java/com/spring/demo/Application.java
+++ src/main/java/com/spring/demo/Application.java
@@ -1,9 +1,17 @@
 package com.spring.demo;
 
 
+import com.spring.demo.service.UserDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.*;
+
 public class Application {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
+        UserDao userDao = applicationContext.getBean(UserDao.class);
+        userDao.dummy();
     }
 }
---
+++ src/main/java/com/spring/demo/service/UserDao.java
@@ -0,0 +1,28 @@
+package com.spring.demo.service;
+
+
+import com.mysql.jdbc.Connection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Service
+public class UserDao {
+
+    @Autowired
+    DataSource datasource;
+
+    public void dummy() throws SQLException {
+        Connection connection = (Connection) datasource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+    }
+}
